f4d6317149ff04eab9a828c5b38914a29b140311
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-04-20 18:48:18 +0100

    declared array

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,12 @@ const generateBtn = document.querySelector("#generate");
 
 // main function to generate the random password // All your code goes here to generate a password
 const generatePassword = () => {
-  /* declare all password criteria 
- lowercase "abcdefghijklmnopqrstuvwxyz"
- uppercase "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
- numeric "0123456789"
- special characters " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+  //declare all password criteria 
+
+ const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+ const uppercase =  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+ const numeric = ["0","1","2","3","4","5","6","7","8","9"];
+ const specialCharacters = [" ","!","\",""","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
 
    *prompt user to enter password length
    */
