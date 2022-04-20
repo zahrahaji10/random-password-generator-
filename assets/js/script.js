39eba740efb2e1bcede296ff1865dc55bc355bb6
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-04-20 20:42:40 +0100

    inital prompt for password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
-// main function to generate the random password // All your code goes here to generate a password
+// main function to generate the random password
+
 const generatePassword = () => {
-  //declare all password criteria 
+  //All your code goes here to generate a password
 
- const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
- const uppercase =  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
- const numeric = ["0","1","2","3","4","5","6","7","8","9"];
- const specialCharacters = [" ","!","\",""","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
-
-   *prompt user to enter password length
-   */
+  // prompt user to enter password lenth
+  const passwordLength = prompt(
+    "Please enter a number of how many characters you want your password. Ensure a number between 8 and 128."
+  );
 };
 
 // Write password to the #password input
