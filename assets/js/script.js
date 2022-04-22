30314e60c090b686413a865fb1678a2c774bf42a
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-04-22 15:16:08 +0100

    compeleted password criteria stage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,8 @@
 const generateBtn = document.querySelector("#generate");
 
 // main function to generate the random password
-
 const generatePassword = () => {
-  //(3rd-js then goes to execute this function generate password and reads the code)
-  //All your code goes here to generate a password
-
-  // prompt user
+  // prompt user to get the password length
   function getPasswordLength() {
     const passwordLength = prompt(
       "How many characters do you want your password to have. Please enter between 8 and 128 characters"
@@ -21,59 +17,70 @@ const generatePassword = () => {
     }
   }
 
-  // calling the password length function
+  // calling the get password length function
   const passwordLength = getPasswordLength();
 
-  // declaring variables for all criteria
-  const hasUppercase = confirm(
-    "Would you like to have uppercase characters in you password"
-  );
-  const hasLowercase = confirm(
-    "Would you like to have lowercase characters in you password"
-  );
-  const hasnumeric = confirm(
-    "Would you like to have numeric characters in you password"
-  );
-  const hasSpecialCharacter = confirm(
-    "Would you like to have uppercase characters in you password"
-  );
-
-  // create a random password function
-  const createRandomPassword = () => {
-    let passwordCriteriaArray = [
-      hasUppercase,
-      hasLowercase,
-      hasnumeric,
-      hasSpecialCharacter,
-    ];
-    let finalRandomPasswordArray = [];
-
-    if (passwordCriteriaArray[0]) {
-      finalRandomPasswordArray.push(uppercase);
-    }
-  };
-  createRandomPassword();
-  //declare all password criteria
-
+  // declaring password criteria
   const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const lowercase = "abcdefghijklmnopqrstuvwxyz";
   const numeric = "0123456789";
   const specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-};
 
-//   const getPasswordLength = prompt(
-//     "How many characters do you want your password to have. Please enter between 8 and 128 characters"
-//   );
-//   // convert the string into the correct input value of integer
-//   let convertPasswordLength;
-//   /*Password needs to be between a number greater than or equal to 8 and less than or equal to 128
-//   check password length */
-//  if (parseInt(getPasswordLength, 10) >=8 && parseInt(getPasswordLength, 10) <=128){
-//     return alert("")
-//  } else{
-//     return prompt("Please enter a valid number")
-//  };
-// };
+  //prompting user to select choices of the password criteria
+  const getPasswordCriteria = () => {
+    // declaring variables for all criteria
+    const hasUppercase = confirm(
+      "Would you like to have uppercase characters in you password"
+    );
+    const hasLowercase = confirm(
+      "Would you like to have lowercase characters in you password"
+    );
+    const hasnumeric = confirm(
+      "Would you like to have numeric characters in you password"
+    );
+    const hasSpecialCharacter = confirm(
+      "Would you like to have uppercase characters in you password"
+    );
+
+    // create a random password function
+    const createRandomPassword = () => {
+      const finalRandomPasswordArray = [];
+      const passwordCriteriaArray = [
+        hasUppercase,
+        hasLowercase,
+        hasnumeric,
+        hasSpecialCharacter,
+      ];
+
+      // checking the confirmed criteria of the array
+      const checkCriteria = passwordCriteriaArray.find(
+        (eachItemsWithinArray) => eachItemsWithinArray === true
+      );
+      // pushing confirmed criteria's into final array to generate random password
+      if (checkCriteria) {
+        if (passwordCriteriaArray[0]) finalRandomPasswordArray.push(uppercase);
+
+        if (passwordCriteriaArray[1]) finalRandomPasswordArray.push(lowercase);
+
+        if (passwordCriteriaArray[2]) finalRandomPasswordArray.push(numeric);
+
+        if (passwordCriteriaArray[3])
+          finalRandomPasswordArray.push(specialCharacters);
+      } else {
+        alert("Please select at least one criteria for your password");
+        getPasswordCriteria();
+      }
+      // returning function with final password array
+      return finalRandomPasswordArray;
+    };
+
+    // calling create random password function
+    return createRandomPassword();
+  };
+
+  //calling get password criteria function
+  getPasswordCriteria();
+};
 
 // declare the password criteria
 
