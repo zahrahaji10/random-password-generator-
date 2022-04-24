c46cbf6729ffdebf2cd42f5641ff90ba4ae15fe2
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-04-24 11:18:04 +0100

    updated code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# random-password-generator-
-creating a password generator using Javascript 
+# Random Password Generator
+
+## Description
+
+The use of common and simple passwords increases the risk for hackers to get access to of personal information of individuals. This project utilises HTML and CSS as well as using mainly the programing language of JavaScript to generate a highly secure random password. The user is prompted with a initial question about the desired length of the password which is followed by a number of questions of different criteria's to include in password.
+
+## Application of Deployed Webpage
+
+## Screenshots
+
+## Technologies
+
+-HTML
+-CSS
+-JavaScript
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,108 +8,99 @@ const numeric = "0123456789";
 const specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 let userPasswordConfirms;
 
+// prompt user password length input and validate
+const getPasswordLength = () => {
+  const passwordLength = prompt(
+    "How many characters do you want your password to have. Please enter between 8 and 128 characters"
+  );
+  const passLength = parseInt(passwordLength, 10);
+  if (passLength >= 8 && passLength <= 128) {
+    // return the password length
+    return passLength;
+  } else {
+    alert("Please enter a valid number");
+    return;
+  }
+};
+
+//prompting user to select the password criteria's
+const passwordCriteria = () => {
+  const finalRandomPasswordArray = [];
+  // declaring variables for all criteria
+  const hasUppercase = confirm(
+    "Would you like to have uppercase characters in you password"
+  );
+  const hasLowercase = confirm(
+    "Would you like to have lowercase characters in you password"
+  );
+  const hasNumeric = confirm(
+    "Would you like to have numeric characters in you password"
+  );
+  const hasSpecialCharacter = confirm(
+    "Would you like to have uppercase characters in you password"
+  );
+  // check that at least one of the option was selected
+  if (hasLowercase || hasUppercase || hasSpecialCharacter || hasNumeric) {
+    if (hasUppercase) {
+      finalRandomPasswordArray.push(uppercase);
+    }
+
+    if (hasLowercase) {
+      finalRandomPasswordArray.push(lowercase);
+    }
+
+    if (hasNumeric) {
+      finalRandomPasswordArray.push(numeric);
+    }
+
+    if (hasSpecialCharacter) {
+      finalRandomPasswordArray.push(specialCharacters);
+    }
+    // returning function with final password array
+    return finalRandomPasswordArray;
+  } else {
+    // start over if they don't select anything
+    alert("Please select at least one criteria for your password");
+    passwordCriteria();
+  }
+};
+
 // main function to generate the random password
 const generatePassword = () => {
   // calling the get password length function
   const passwordLength = getPasswordLength();
 
-  // prompt user password length input and validate
-  function getPasswordLength() {
-    const passwordLength = prompt(
-      "How many characters do you want your password to have. Please enter between 8 and 128 characters"
-    );
-    const convertPasswordLength = parseInt(passwordLength, 10);
-    if (convertPasswordLength >= 8 && convertPasswordLength <= 128) {
-      return convertPasswordLength;
-    } else {
-      alert("Please enter a valid number");
-      return;
-    }
-  }
-
-  //prompting user to select the password criteria's
-  const passwordCriteria = () => {
-    // declaring variables for all criteria
-    const hasUppercase = confirm(
-      "Would you like to have uppercase characters in you password"
-    );
-    const hasLowercase = confirm(
-      "Would you like to have lowercase characters in you password"
-    );
-    const hasnumeric = confirm(
-      "Would you like to have numeric characters in you password"
-    );
-    const hasSpecialCharacter = confirm(
-      "Would you like to have uppercase characters in you password"
-    );
-
-    // create a random password function
-    const createRandomPassword = () => {
-      const finalRandomPasswordArray = [];
-      const passwordCriteriaArray = [
-        hasUppercase,
-        hasLowercase,
-        hasnumeric,
-        hasSpecialCharacter,
-      ];
-
-      // checking the confirmed criteria of the array
-      const checkCriteria = passwordCriteriaArray.find(
-        (eachItemsWithinArray) => eachItemsWithinArray === true
+  if (passwordLength) {
+    //calling get password criteria function
+    const passwordRandomArray = passwordCriteria();
+    let password = "";
+    // loop for selecting random category from user password confirms
+    for (let i = 0; i < passwordLength; i++) {
+      //creating a index to get a random category
+      const randomCategoryIndex = Math.floor(
+        Math.random() * passwordRandomArray.length
       );
-      // pushing confirmed criteria's into final array to generate random password
-      if (checkCriteria) {
-        if (passwordCriteriaArray[0]) finalRandomPasswordArray.push(uppercase);
-
-        if (passwordCriteriaArray[1]) finalRandomPasswordArray.push(lowercase);
-
-        if (passwordCriteriaArray[2]) finalRandomPasswordArray.push(numeric);
-
-        if (passwordCriteriaArray[3])
-          finalRandomPasswordArray.push(specialCharacters);
-      } else {
-        alert("Please select at least one criteria for your password");
-        passwordCriteria();
-      }
-
-      // returning function with final password array
-      return finalRandomPasswordArray;
-    };
-
-    // calling create random password function
-    userPasswordConfirms = createRandomPassword();
-  };
-
-  //calling get password criteria function
-  passwordCriteria();
-
-  let password = "";
-  // loop for selecting random category from user password confirms
-  for (let i = 0; i < passwordLength; i++) {
-    //creating a index to get a random category
-    const randomCategoryIndex = Math.floor(
-      Math.random() * userPasswordConfirms.length
-    );
-    //using the index to get a string of characters from confirmed user password confirms array
-    const confirmedPasswordCriteriaArray =
-      userPasswordConfirms[randomCategoryIndex];
-    //splitting each individual string items into an array
-    const categoryArray = confirmedPasswordCriteriaArray.split("");
-    //using a index to randomly select characters from an array
-    const randomIndex = Math.floor(Math.random() * categoryArray.length);
-    //joining the separate randomly selected characters array into a password variable
-    password += categoryArray[randomIndex];
+      //using the index to get a string of characters from confirmed user password confirms array
+      const confirmedPasswordCriteriaArray =
+        passwordRandomArray[randomCategoryIndex];
+      //splitting each individual string items into an array
+      const categoryArray = confirmedPasswordCriteriaArray.split("");
+      //using a index to randomly select characters from an array
+      const randomIndex = Math.floor(Math.random() * categoryArray.length);
+      //joining the separate randomly selected characters array into a password variable
+      password += categoryArray[randomIndex];
+    }
+    // returning the generated password string
+    return password;
+  } else {
+    return;
   }
-  // returning the string of selected characters array to display as password
-  return password;
 };
-
-// declare the password criteria
 
 //Write password to the #password input
 const writePassword = () => {
   //(2nd-js then goes to execute this function generate password and reads the code)
-  const password = generatePassword();
+  const password = generatePassword() || "";
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
